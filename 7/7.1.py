06168a6fc6ede8bbc168841105f86f4ba9640e7a
Pauline Lettinga <paulinelettinga@gmail.com>
2024-12-07 16:03:53 +0100

    even op weg geholpen door Chat voor part 1, maar daarna was part 2 echt easy

---
+++ 7/7.1.py
@@ -0,0 +1,26 @@
+total = 0
+
+
+def makecombinations(n):
+    if n == 0:
+        return [[]]
+    combinations = makecombinations(n-1)
+    return [combo + ["+"] for combo in combinations] + [combo + ["*"] for combo in combinations]
+
+
+with open('data.txt') as f:
+    for line in f:
+        line = line.strip().split(' ')
+        line[0] = line[0].strip(':')
+        operators = makecombinations(len(line)-2)
+        for ops in operators:
+            result = int(line[1])
+            for number, op in zip(line[2:], ops):
+                if op == '+':
+                    result += int(number)
+                elif op == '*':
+                    result *= int(number)
+            if int(result) == int(line[0]):
+                total += result
+                break
+print(total)
